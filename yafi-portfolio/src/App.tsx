a4d4408ea955c77408666335237b8b9f0ca14268
alyafi0899 <wenyafi@gmail.com>
2026-09-21 12:43:47 +0700

    Update App.tsx

--- yafi-portfolio/src/App.tsx
+++ yafi-portfolio/src/App.tsx
@@ -743,7 +743,8 @@ function AboutPage({ navigate }: { navigate: (p: Page) => void }) {
           className="about-hero-full-img"
         />
         <div className="about-hero-full-content">
-          <h1 className="about-hero-full-title">Hello!\nI'm  Al Yafi</h1>
+          <h1 className="about-hero-full-title">Hello!</h1>
+          <h1 className="about-hero-full-title">I'm  Al Yafi</h1> 
         </div>
       </section>
 
